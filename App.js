9f300c1a0452995b1b66a4d84f8cb64707e13e1e
yash1709 <95284297+yash1709@users.noreply.github.com>
2023-08-02 02:00:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "./Navbar";
+import Main from "./Main";
+
+function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
+
+export default App;
---
+++ Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav>
+            <img src="" alt="react-icon-image" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
+
+export default Navbar;
